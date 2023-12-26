b8fb07a8d3c3854716634ee9e9fca21ace6d0d98
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-12-26 12:15:19 +0100

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    template: `<app-header></app-header>`,
-    styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+  selector: 'app-root',
+  standalone: true,
+  template: `<app-header></app-header><router-outlet></router-outlet>`,
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   title = 'shopper';
