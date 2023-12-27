b5ca568dfbc2c9cacbeb52de4fe0df35a7e7fa3c
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-12-27 21:27:22 +0100

    update row height dynamic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,6 +11,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { ProductsHeaderComponent } from './components/products-header/products-header.component';
 import { FiltersComponent } from './components/filters/filters.component';
 
+const ROWS_HEIGHT: {[id: number]: number} = { 1: 400, 3: 335, 4: 350}
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -32,10 +34,12 @@ import { FiltersComponent } from './components/filters/filters.component';
 })
 export class HomeComponent {
   cols: number = 3;
+  rowHeight:number = ROWS_HEIGHT[this.cols]
   category: string | undefined;
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols]
   }
 
   onShowCategory(newCategory: string): void {
